684316f00d570a90dc7f2dc3cc92c2828efac2d8
Luca Giovannesi <l.giovannesi@gmail.com>
2022-09-01 19:21:16 +0200

    Club details

--- app/src/main/java/com/hackerini/discoticket/objects/Club.kt
+++ app/src/main/java/com/hackerini/discoticket/objects/Club.kt
@@ -6,5 +6,6 @@ class Club : Serializable {
     var name: String = ""
     var address: String = ""
     var rating: Float = 0.0F
+    var reviewAmount = (10..100).random()
     var imgUrl: String = "https://img.freepik.com/free-vector/disco-ball-background_1284-5130.jpg"
 }
