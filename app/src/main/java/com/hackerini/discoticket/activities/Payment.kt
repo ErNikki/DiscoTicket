11078408e47f257adb556524af97c6a2f69b3691
Zakaehl <onafets24@gmail.com>
2022-09-03 11:50:01 +0200

    Beginning payment

--- app/src/main/java/com/hackerini/discoticket/activities/Payment.kt
+++ app/src/main/java/com/hackerini/discoticket/activities/Payment.kt
@@ -1,8 +1,15 @@
 package com.hackerini.discoticket.activities
 
+import android.graphics.Paint
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.RectShape
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
 import com.hackerini.discoticket.R
 import com.hackerini.discoticket.objects.OrderPreview
 
@@ -10,18 +17,32 @@ class Payment : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
-        val orderPreview = intent.getSerializableExtra("OrderPreview") as OrderPreview
 
-        orderPreview.items.forEach { e ->
-            Log.d("TAG", e.name)
-            /*For each element {e} you have:
-            e.name
-            e.quantity
-            e.unitaryPrice //If needed
-            e.getTotalAmount() //To get the total amount of each drink
-             */
+        findViewById<ScrollView>(R.id.purchaseDetails).addBorder()          //Add border to the list container
+
+        val orderPreview = intent.getSerializableExtra("OrderPreview") as OrderPreview
+        val purchaseList = findViewById<LinearLayout>(R.id.purchaseList)
+        orderPreview.items.forEach { e ->                   //Add elements about to be purchased to the list
+            val listElement = TextView(this)
+            listElement.text = " " + e.quantity.toString() + "x " + e.name + " - " +
+                                String.format("%.2f", e.getTotalAmount()) + "€"
+            listElement.textSize = 20f
+            listElement.addBorder()
+            purchaseList.addView(listElement)
         }
 
-        orderPreview.getTotalAmount() //Computes the total amount of the order
+        findViewById<TextView>(R.id.purchaseTotal).append(String.format("%.2f", orderPreview.getTotalAmount()) + "€")
     }
+}
+
+//Function for adding a border to the list container and to the elements of the list
+fun View.addBorder() {
+    val drawable = ShapeDrawable().apply {
+        shape = RectShape()
+        paint.apply {
+            strokeWidth = 5F                //Border width
+            style = Paint.Style.STROKE
+        }
+    }
+    background = drawable
 }
