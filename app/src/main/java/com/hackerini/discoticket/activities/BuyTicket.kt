42123e9f47d904009f0a4ea58e2e0dcaafeb98a8
Luca Giovannesi <l.giovannesi@gmail.com>
2022-09-03 18:13:36 +0200

    BuyTicket screen implemented

--- app/src/main/java/com/hackerini/discoticket/activities/BuyTicket.kt
+++ app/src/main/java/com/hackerini/discoticket/activities/BuyTicket.kt
@@ -1,71 +1,216 @@
 package com.hackerini.discoticket.activities
 
+import android.app.ActionBar
 import android.graphics.Color
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import com.hackerini.discoticket.R
 import com.hackerini.discoticket.fragments.views.DayViewContainer
+import com.hackerini.discoticket.fragments.views.MonthViewContainer
 import com.hackerini.discoticket.objects.Club
+import com.hackerini.discoticket.objects.Event
 import com.kizitonwose.calendarview.CalendarView
 import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.model.CalendarMonth
 import com.kizitonwose.calendarview.model.DayOwner
 import com.kizitonwose.calendarview.ui.DayBinder
+import com.kizitonwose.calendarview.ui.MonthHeaderFooterBinder
 import java.time.DayOfWeek
+import java.time.LocalDate
 import java.time.YearMonth
+import java.time.format.TextStyle
 import java.time.temporal.WeekFields
 import java.util.*
 
 class BuyTicket : AppCompatActivity() {
 
-    val daysOfWeek = arrayOf(
-        DayOfWeek.SUNDAY,
-        DayOfWeek.MONDAY,
-        DayOfWeek.TUESDAY,
-        DayOfWeek.WEDNESDAY,
-        DayOfWeek.THURSDAY,
-        DayOfWeek.FRIDAY,
-        DayOfWeek.SATURDAY
-    )
+    private var selectedDate: CalendarDay? = null
+    private var amountOfSimpleTicket = 0
+    private var amountOfTableTicket = 0
+    private var club: Club? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buy_ticket)
 
-        val club = intent.getSerializableExtra("club") as Club
+        club = intent.getSerializableExtra("club") as Club
 
         val clubName = findViewById<TextView>(R.id.BuyTicketClubName)
         val clubAddress = findViewById<TextView>(R.id.BuyTicketClubAddress)
+        val simpleTicketPrice = findViewById<TextView>(R.id.BuyTicketSimpleTicketPrice)
+        val tableTicketPrice = findViewById<TextView>(R.id.BuyTicketTableTicketPrice)
+        val payButton = findViewById<Button>(R.id.BuyTicketPayButton)
+        val viewTable = findViewById<Button>(R.id.BuyTicketViewTableButton)
+        val simpleTicketCounter = findViewById<EditText>(R.id.BuyTicketAmountSimpleTicket)
+        val tableTicketCounter = findViewById<EditText>(R.id.BuyTicketAmountTableTicket)
 
-        clubName.text = club.name
-        clubAddress.text = club.address
+        clubName.text = club?.name
+        clubAddress.text = club?.address
+        simpleTicketPrice.text = String.format("%.2f", club?.simpleTicketPrice) + "€/Persona"
+        tableTicketPrice.text = String.format("%.2f", club?.tableTicketPrice) + "€/Persona"
 
         val calendarView = findViewById<CalendarView>(R.id.BuyTicketCalendar)
-
         calendarView.dayBinder = object : DayBinder<DayViewContainer> {
+
             // Called only when a new container is needed.
-            override fun create(view: View) = DayViewContainer(view)
+            override fun create(view: View): DayViewContainer {
+                return DayViewContainer(view, calendarView)
+            }
 
             // Called every time we need to reuse a container.
+            var lastHighlighted: TextView? = null
+
             override fun bind(container: DayViewContainer, day: CalendarDay) {
+                val textView = container.textView
+                container.day = day
+                textView.text = day.date.dayOfMonth.toString()
+                textView.visibility = View.VISIBLE
+
+                val shape = GradientDrawable()
+                shape.cornerRadius = 20F
+                shape.setColor(Color.LTGRAY)
+
+
                 if (day.owner == DayOwner.THIS_MONTH) {
-                    container.textView.text = day.date.dayOfMonth.toString()
-                    container.textView.setTextColor(Color.BLACK)
+                    val isOpened = day.date.dayOfWeek.ordinal == 5
+                    val isFuture =
+                        day.date.isAfter(LocalDate.now()) || day.date.isEqual(LocalDate.now())
+                    if (day.date == container.selectedDate && isOpened && isFuture) {
+                        selectedDate = day
+                        if (amountOfTableTicket > 0 || amountOfSimpleTicket > 0)
+                            payButton.isEnabled = true
+                        lastHighlighted?.setTextColor(Color.BLACK)
+                        lastHighlighted?.background = null
+
+                        textView.setTextColor(Color.WHITE)
+                        textView.background = shape
+                        lastHighlighted = textView
+                    } else if (!isOpened || !isFuture) {
+                        textView.setTextColor(Color.GRAY)
+                        textView.background = null
+                    } else {
+                        textView.setTextColor(Color.BLACK)
+                        textView.background = null
+                    }
+                } else {
+                    textView.setTextColor(Color.LTGRAY)
                 }
-                else
-                    container.textView.visibility=View.INVISIBLE
-                    //container.textView.setTextColor(Color.GRAY)
+            }
+        }
+
+        calendarView.monthHeaderBinder = object : MonthHeaderFooterBinder<MonthViewContainer> {
+            override fun create(view: View) = MonthViewContainer(view)
+            override fun bind(container: MonthViewContainer, month: CalendarMonth) {
+                container.textView.text =
+                    month.yearMonth.month.getDisplayName(TextStyle.FULL, Locale.getDefault())
+                        .replaceFirstChar(Char::uppercase) + " " + month.year
             }
         }
 
         val currentMonth = YearMonth.now()
         val firstMonth = currentMonth.minusMonths(10)
         val lastMonth = currentMonth.plusMonths(10)
-        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
-        calendarView.setup(firstMonth, lastMonth, firstDayOfWeek)
+        val firstDayOfWeek = daysOfWeekFromLocale()
+        val locale = Locale.getDefault()
+        calendarView.setup(firstMonth, lastMonth, firstDayOfWeek.first())
         calendarView.scrollToMonth(currentMonth)
+
+        val dayOfWeekLLayout = findViewById<LinearLayout>(R.id.BuyTicketDayOfWeekLLayout)
+        firstDayOfWeek.forEach { e ->
+            val textView = TextView(this)
+            textView.text =
+                e.getDisplayName(TextStyle.SHORT, locale).replaceFirstChar(Char::uppercase)
+            val param = LinearLayout.LayoutParams(
+                ActionBar.LayoutParams.MATCH_PARENT,
+                ActionBar.LayoutParams.MATCH_PARENT,
+                1.0f
+            )
+            textView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            textView.layoutParams = param
+            textView.setTypeface(null, Typeface.BOLD)
+            dayOfWeekLLayout.addView(textView)
+        }
+
+        payButton.setOnClickListener {
+            Log.d("BOOK", "BOOKED FOR " + selectedDate.toString())
+        }
+        simpleTicketCounter.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                amountOfSimpleTicket = s.toString().toIntOrNull() ?: 0
+                updateCartTotal()
+            }
+        })
+        tableTicketCounter.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {}
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int,
+                count: Int, after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int,
+                before: Int, count: Int
+            ) {
+                amountOfTableTicket = s.toString().toIntOrNull() ?: 0
+                updateCartTotal()
+            }
+        })
     }
+
+    fun updateCartTotal() {
+        val payButton = findViewById<Button>(R.id.BuyTicketPayButton)
+        val viewTable = findViewById<Button>(R.id.BuyTicketViewTableButton)
+        val totalAmountView = findViewById<TextView>(R.id.BuyTicketTotalAmount)
+        if (this.amountOfTableTicket > 0) {
+            viewTable.setText("Seleziona tavolo")
+            payButton.visibility = View.GONE
+        } else {
+            viewTable.setText("Vedi tavoli")
+            payButton.visibility = View.VISIBLE
+        }
+        club?.let {
+            val amount =
+                it.simpleTicketPrice * amountOfSimpleTicket + it.tableTicketPrice * amountOfTableTicket
+            totalAmountView.setText(String.format("%.2f", amount) + "€")
+        }
+
+        if ((amountOfTableTicket > 0 || amountOfSimpleTicket > 0) && selectedDate!=null)
+            payButton.isEnabled = true
+    }
+
+    fun daysOfWeekFromLocale(): Array<DayOfWeek> {
+        val firstDayOfWeek = WeekFields.of(Locale.getDefault()).firstDayOfWeek
+        val daysOfWeek = DayOfWeek.values()
+        // Order `daysOfWeek` array so that firstDayOfWeek is at index 0.
+        // Only necessary if firstDayOfWeek is not DayOfWeek.MONDAY which has ordinal 0.
+        if (firstDayOfWeek != DayOfWeek.MONDAY) {
+            val rhs = daysOfWeek.sliceArray(firstDayOfWeek.ordinal..daysOfWeek.indices.last)
+            val lhs = daysOfWeek.sliceArray(0 until firstDayOfWeek.ordinal)
+            return rhs + lhs
+        }
+        return daysOfWeek
+    }
+
 }
--- app/src/main/java/com/hackerini/discoticket/fragments/views/DayViewContainer.kt
+++ app/src/main/java/com/hackerini/discoticket/fragments/views/DayViewContainer.kt
@@ -1,13 +1,54 @@
 package com.hackerini.discoticket.fragments.views
 
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import com.hackerini.discoticket.R
+import com.kizitonwose.calendarview.CalendarView
+import com.kizitonwose.calendarview.model.CalendarDay
+import com.kizitonwose.calendarview.model.DayOwner
 import com.kizitonwose.calendarview.ui.ViewContainer
+import java.time.LocalDate
 
-class DayViewContainer(view: View) : ViewContainer(view) {
+class DayViewContainer(view: View, calendarView: CalendarView) : ViewContainer(view) {
     val textView = view.findViewById<TextView>(R.id.calendarDayText)
 
-    // With ViewBinding
-    // val textView = CalendarDayLayoutBinding.bind(view).calendarDayText
+    // Will be set when this container is bound
+    lateinit var day: CalendarDay
+    var selectedDate: LocalDate? = null
+
+    init {
+        view.setOnClickListener {
+            //selectedDate?.let { it1 -> calendarView.notifyDateChanged(it1) }
+            // Check the day owner as we do not want to select in or out dates.
+            if (day.owner == DayOwner.THIS_MONTH) {
+                // Keep a reference to any previous selection
+                // in case we overwrite it and need to reload it.
+                //Log.d("HERE",day.date.toString());
+                day.date.let { it1 ->
+                    calendarView.notifyDateChanged(it1)
+                    selectedDate=it1
+                }
+                /*val currentSelection = selectedDate
+                if (currentSelection == day.date) {
+                    // If the user clicks the same date, clear selection.
+                    selectedDate = null
+                    // Reload this date so the dayBinder is called
+                    // and we can REMOVE the selection background.
+                    calendarView.notifyDateChanged(currentSelection)
+                } else {
+                    selectedDate = day.date
+                    // Reload the newly selected date so the dayBinder is
+                    // called and we can ADD the selection background.
+                    calendarView.notifyDateChanged(day.date)
+                    if (currentSelection != null) {
+                        // We need to also reload the previously selected
+                        // date so we can REMOVE the selection background.
+                        calendarView.notifyDateChanged(currentSelection)
+                    }
+                }*/
+            }
+        }
+    }
+
 }
