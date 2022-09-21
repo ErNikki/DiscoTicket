97428ec54b558c31c8d8c00ef6553dd47de13dc3
Luca Giovannesi <l.giovannesi@gmail.com>
2022-09-21 15:36:13 +0200

    Points amount bold

--- app/src/main/java/com/hackerini/discoticket/ui/home/MyCoupon.kt
+++ app/src/main/java/com/hackerini/discoticket/ui/home/MyCoupon.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.text.HtmlCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.hackerini.discoticket.R
@@ -112,7 +113,10 @@ class MyCoupon : Fragment() {
     fun updateValues() {
         val user = User.getLoggedUser(requireContext())
         username.text = user!!.name + " " + user!!.surname
-        pointsRecup.text = "Hai un totale di ${user!!.points} punti"
+        pointsRecup.text = HtmlCompat.fromHtml(
+            "Hai un totale di <b>${user!!.points}</b> punti",
+            HtmlCompat.FROM_HTML_MODE_LEGACY
+        )
         pointExplain.text =
             "Ogni ${resources.getInteger(R.integer.pointToEuroThreshold)} punti accumulati hai diritto a ricevere un buono di "
                 .plus(String.format("%.2f", valueOfConvertedPoints))
