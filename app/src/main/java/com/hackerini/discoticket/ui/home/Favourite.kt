c901f5afa0806b928743d8e0df70807f2f537d2d
Luca Giovannesi <l.giovannesi@gmail.com>
2022-09-22 19:42:25 +0200

    Animation and removing done, some problems to fix

--- app/src/main/java/com/hackerini/discoticket/ui/home/Favourite.kt
+++ app/src/main/java/com/hackerini/discoticket/ui/home/Favourite.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import com.hackerini.discoticket.R
 import com.hackerini.discoticket.fragments.elements.DiscoElement
 import com.hackerini.discoticket.room.RoomManager
@@ -15,6 +16,8 @@ import com.hackerini.discoticket.utils.ObjectLoader
 class Favourite : Fragment() {
 
     lateinit var emptyScreenWarning: TextView
+    lateinit var linearLayout: LinearLayout
+    private var fragmentList: MutableList<DiscoElement> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,27 +40,58 @@ class Favourite : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        linearLayout = view.findViewById(R.id.FavoriteLinearLayout)
         emptyScreenWarning = view.findViewById(R.id.FavoriteClubEmptyWarning)
-
     }
 
     override fun onResume() {
         super.onResume()
+        linearLayout.removeAllViews()
+        loadContent()
+    }
+
+    private fun loadContent() {
         val favClubs = RoomManager(requireContext()).db.favoriteDao().getAll()
-        view?.findViewById<LinearLayout>(R.id.FavoriteLinearLayout)?.removeAllViews()
-        if (favClubs.isEmpty()) {
-            emptyScreenWarning.visibility = View.VISIBLE
-        }
-        if (favClubs.isNotEmpty()) {
-            emptyScreenWarning.visibility = View.GONE
-            val ft = parentFragmentManager.beginTransaction()
-            val clubs = ObjectLoader.getClubs(requireContext())
-            clubs.forEach { club ->
-                if (favClubs.map { e -> e.id }.contains(club.id)) {
-                    ft.add(R.id.FavoriteLinearLayout, DiscoElement.newInstance(club))
+        val favClubsIds = favClubs.map { e -> e.id }
+        fragmentList = ObjectLoader.getClubs(requireContext())
+            .filter { club -> favClubsIds.contains(club.id) }
+            .map { club ->
+                DiscoElement.newInstance(club, true).apply {
+                    onRemoveElement = { c -> removeElement(this) }
                 }
+            }.toMutableList()
+
+        updateWarningTextStatus()
+        if (fragmentList.isNotEmpty()) {
+            val transaction = parentFragmentManager.beginTransaction()
+            fragmentList.forEach {
+                transaction.add(linearLayout.id, it)
             }
-            ft.commit()
+            transaction.commit()
         }
+    }
+
+    private fun removeElement(element: DiscoElement) {
+        //Element are not removed really when deleted, but they are only hidden, in this way is easier
+        //to recover a fragment after the delete
+        element.hide { it.removeToFavorite(requireContext()) }
+        updateWarningTextStatus()
+
+        Snackbar.make(
+            requireView().findViewById(R.id.FavoriteConstraintLayout),
+            element.club?.name + " eliminato dai preferiti",
+            Snackbar.LENGTH_SHORT
+        ).setAction("Annulla") {
+            element.show { element.club?.addToFavorite(requireContext()) }
+            updateWarningTextStatus()
+        }
+            .show()
+    }
+
+    private fun updateWarningTextStatus() {
+        if (fragmentList.all { e -> e.isHide }) {
+            emptyScreenWarning.visibility = View.VISIBLE
+
+        } else emptyScreenWarning.visibility = View.GONE
     }
 }
